ef0e9e0187eb5c13fa8ac12ff57f17e16ba7fa22
bassupreme <leonardo.basso.7@studenti.unipd.it>
2023-05-16 19:27:26 +0200

    close #5

---
+++ roman-number/src/main/java/it/unipd/mtss/NumberAEFiveThousand.java
@@ -0,0 +1,12 @@
+////////////////////////////////////////////////////////////////////
+// CARLO ROSSO 2034293 
+// LEONARDO BASSO 2042329 
+////////////////////////////////////////////////////////////////////
+package it.unipd.mtss;
+
+public class NumberAEFiveThousand extends Exception {
+
+    public NumberAEFiveThousand() {
+        super("Non esistono numeri romani maggiori di 4999. ");
+    }
+}
